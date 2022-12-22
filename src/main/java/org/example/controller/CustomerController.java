8153b294fb76b8b0b4befcb77d7778c21de522ab
Nico <nicomenzii@gmail.com>
2022-12-22 16:48:31 +0100

    add CustomerController.java with getAllCustomers mapping which returns all customers in the Database

---
+++ src/main/java/org/example/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package org.example.controller;
+
+import org.example.model.Customer;
+import org.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+}
